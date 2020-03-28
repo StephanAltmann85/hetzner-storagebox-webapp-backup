758db4d3e27699b633377dfe4b18c9581f013f77
Stephan Altmann <salty@Lackmaus.local>
2020-03-28 21:58:07 +0100

    added const for public key path

--- backup_authorize.sh
+++ backup_authorize.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 TARGET_SERVER="username.your-storagebox.de"
 TARGET_USER=""
+PUB_KEY_PATH="~/.ssh/id_rsa.pub"
 
 CAT=`which cat`
 SFTP=`which sftp`
 
-${CAT} ~/.ssh/id_rsa.pub > authorized_keys
+${CAT} $PUB_KEY_PATH > authorized_keys
 echo "mkdir database \n mkdir files\n mkdir .ssh \n chmod 700 .ssh \n put authorized_keys .ssh/authorized_keys \n chmod 600 .ssh/authorized_keys" | ${SFTP} $TARGET_USER@$TARGET_SERVER
 wait
 rm authorized_keys
